7395e7b897ab9899288b5f6f8b90d1a4190d3d0b
Ahmed Hannan <ahmedhannansidra2@hotmail.com>
2023-12-22 19:20:37 +0500

    Add coloring functionality and a button to create a new custom sized grid

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 const grid = document.querySelector("#grid");
+const createGridButton = document.querySelector("#create-grid-btn");
 
-const GRID_SIZE = 16;
+createGridButton.addEventListener("click", () => {
+    let gridSize;
+    do {
+        gridSize = prompt("Enter Grid Size (Max 100): ", 16);
+    } while (gridSize < 1 || gridSize > 100 || gridSize === null);
+
+    createGrid(gridSize);
+});
+
+const DEFAULT_GRID_SIZE = 16;
 
 // Creates a grid of specified number of divs
 function createGrid(gridSize) {
+    clearGrid(); // Clears the grid to avoid any problems
+
     for (let i = 0; i < gridSize; i++) {
         // Creates a grid row
         let gridRow = document.createElement("div");
@@ -13,11 +25,22 @@ function createGrid(gridSize) {
         for (let j = 0; j < gridSize; j++) {
             let gridSquare = document.createElement("div");
             gridSquare.classList.add("grid-square");
+
+            gridSquare.addEventListener("mouseenter", () => {
+                gridSquare.style.backgroundColor = "red";
+            });
+
             gridRow.appendChild(gridSquare);
             grid.appendChild(gridRow);
         }
     }
 }
 
+function clearGrid() {
+    while (grid.lastElementChild) {
+        grid.removeChild(grid.lastElementChild);
+    }
+}
+
 // Create the grid
-createGrid(GRID_SIZE);
+createGrid(DEFAULT_GRID_SIZE);
